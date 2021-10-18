5c9e8567b808716ec7525febaf6c6e13f03f162b
SMei201900 <smei201900@gmail.com>
2021-10-18 13:17:29 -0400

    Added Credits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import Credits from "./Credits";
 
 export default function App() {
 	return (
 		<div className="App">
-			<h1> Recipe App </h1>
+			<header>
+				<h1> Recipe App </h1>
+			</header>
+			<br />
+			<Credits />
 		</div>
 	);
 }
