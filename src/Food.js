a4aed6865254da03c6589d2ac664f0f0141d0469
SMei201900 <smei201900@gmail.com>
2021-10-22 09:18:01 -0400

    Created Form to handle searching
    Temporary and subjected to change

--- src/Food.js
+++ src/Food.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Food() {
+	let [word, setWord] = useState("");
+
+	function handleResponse(response) {
+		console.log(response);
+	}
+
+	function search(event) {
+		event.preventDefault();
+		let apiKey = ``;
+		let apiURL = ``;
+		axios.get(apiURL).then(handleResponse);
+	}
+
+	function handleSearch(event) {
+		setWord(event.target.value);
+	}
+
 	return (
 		<div className="Food">
-			<p> I tried</p>
+			<h1>Type an ingredient and we'll return some recipes for you</h1>
+			<div>
+				<form onSubmit={search}>
+					<input type="search" autoFocus={true} onChange={handleSearch} />
+				</form>
+			</div>
+			<div className="hint">Suggested words: chicken, egg</div>
 		</div>
 	);
 }
