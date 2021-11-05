31ec44f0b3d2bf6156f8318c758e72171cbaa6cf
SMei201900 <smei201900@gmail.com>
2021-11-05 00:46:15 -0400

    Deleted unnecessary comment

--- src/RecipeInfo.js
+++ src/RecipeInfo.js
@@ -1,28 +1,6 @@
 import React from "react";
 
 export default function RecipeInfo(props) {
-	/*
-props equal response.data.hits
-After .hits is 0,1,2,3,4 . . . 
-so we start the loop here 
-SO
-  
-  {
-    props.results.map(function (returns, index) {
-      return (
-        <div key={index}>
-        {returns.recipe.calories}
-        </div>
-    
-      );
-    })
-  }
-
-  NOTE: RETURNS in the MAP FUNCTION should be the 0,1,2,3 numbers 
-
-
-  */
-
 	return (
 		<div className="RecipeInfo">
 			<h1> where are and {props.data.name} </h1>{" "}
